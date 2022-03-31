78c531fe9f127c6e21d07eb97e981b244cda76b4
GoWoong <popgun10@naver.com>
2022-03-31 23:58:23 +0900

    status code

--- kostaurant/foods/views.py
+++ kostaurant/foods/views.py
@@ -2,7 +2,7 @@ from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
-# Create your views here.
+from django.http import Http404
 def index(request):
   today = datetime.today().date()
   context = {"date":today}
@@ -15,4 +15,6 @@ def food_detail(request,food):
     context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격 !"
     context["price"] = 10000
     context["img_path"] = "foods/images/chicken.jpg"
+  else:
+    raise Http404("없는 음식입니다.")
   return render(request, 'foods/detail.html',context=context)
