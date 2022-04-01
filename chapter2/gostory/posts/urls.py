7463103fd50c2b79832d26d422aa118852a79147
GoWoong <popgun10@naver.com>
2022-04-01 18:36:03 +0900

    chapter2 set url path

--- chapter2/gostory/posts/urls.py
+++ chapter2/gostory/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
   # path('', views.index),
-  # path('posts/', views.post_list),
+  path('posts/', views.post_list),
   # path('posts/new', views.post_create),
   # path('posts/<int:post_id>', views.post_detail),
   # path('posts/<int:post_id>/edit', views.post_update),
--- chapter2/gostory/posts/views.py
+++ chapter2/gostory/posts/views.py
@@ -1,3 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from posts.models import Post
 # Create your views here.
+
+def post_list(request):
+  posts = Post.objects.all()
+  context = {"posts": posts}
+  return render(request, 'posts/post_list.html',context)
