ef57ccc0d72c5a6b59c50ea52adf8f04024d5b76
GoWoong <popgun10@naver.com>
2022-04-04 14:05:32 +0900

    form study

--- chapter2/gostory/posts/views.py
+++ chapter2/gostory/posts/views.py
@@ -1,5 +1,5 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from posts.forms import PostForm
 from posts.models import Post
 # Create your views here.
 
@@ -12,3 +12,18 @@ def post_detail(request, post_id):
   post = Post.objects.get(id=post_id)
   context = {"post":post}
   return render(request, 'posts/post_detail.html',context)
+
+def post_create(request):
+  if request.method == "POST":
+    title = request.POST['title']
+    content = request.POST['content']
+    new_post = Post(
+      title = title,
+      content = content
+    )
+    new_post.save()
+    return redirect('post-detail', post_id = new_post.id)
+  else:
+    post_form = PostForm
+    return render(request,'posts/post_form.html',context = {"form":post_form})
+  
