663f24ab4a2e53cdebe28aef85597a5c0ad21da5
GoWoong <popgun10@naver.com>
2022-04-04 14:21:44 +0900

    modelform

--- chapter2/gostory/posts/forms.py
+++ chapter2/gostory/posts/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from .models import Post
+class PostForm(forms.ModelForm):
+  
+  class Meta:
+    model = Post
 
-class PostForm(forms.Form):
-  title = forms.CharField(max_length=50, label="제목")
-  content = forms.CharField(label="내용", widget=forms.Textarea)
+    fields = ['title', 'content'] #'__all__'
+
+
+  # 이전 방식
+  # title = forms.CharField(max_length=50, label="제목")
+  # content = forms.CharField(label="내용", widget=forms.Textarea)
