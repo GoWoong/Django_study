8caae93d680a97976a909a1ace9b71d8743ee1ef
GoWoong <popgun10@naver.com>
2022-04-05 17:55:21 +0900

    USE GenericView - ListView, DeleteView, UpdateView ...

--- chapter2/gostory/posts/views.py
+++ chapter2/gostory/posts/views.py
@@ -1,25 +1,45 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect
 from django.core.paginator import Paginator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse
 
 from posts.forms import PostForm
 from posts.models import Post
 # Create your views here.
 
-def post_list(request):
-  posts = Post.objects.all()
-  paginator = Paginator(posts, 6)
-  curr_page_number = request.GET.get('page')
-  if curr_page_number is None:
-    curr_page_number = 1
-  page = paginator.page(curr_page_number)
-  return render(request, 'posts/post_list.html', {"page":page})
+def index(request):
+  return redirect('post-list')
 
-def post_detail(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  context = {"post":post}
-  return render(request, 'posts/post_detail.html',context)
+  
+# def post_list(request):
+#   posts = Post.objects.all()
+#   paginator = Paginator(posts, 6)
+#   curr_page_number = request.GET.get('page')
+#   if curr_page_number is None:
+#     curr_page_number = 1
+#   page = paginator.page(curr_page_number)
+#   return render(request, 'posts/post_list.html', {"page":page})
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'post/post_list.html'
+  context_object_name = 'posts'
+  ordering = ['-dt_created']
+  paginate_by = 6
+  page_kwarg = 'page'
+
+
+# def post_detail(request, post_id):
+#   post = get_object_or_404(Post, id=post_id)
+#   context = {"post":post}
+#   return render(request, 'posts/post_detail.html',context)
+
+class PostDetailView(DetailView):
+  model = Post
+  template_name = 'posts/post_detail.html'
+  pk_url_kwarg = 'post_id'
+  context_object_name = 'post'
+
 
 # 일반적인 함수형 뷰
 # def post_create(request):
@@ -53,24 +73,38 @@ class PostCreateView(CreateView):
     return reverse('post-detail',kwargs={'post_id':self.object.id})
 
 
-def post_update(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  if request.method == 'POST':
-    post_form = PostForm(request.POST, instance=post)
-    if post_form.is_valid():
-      post_form.save()
-      return redirect('post-detail',post_id = post_id)
-  else:
-    post_form = PostForm(instance=post) #post데이터가 폼에 바운드 된 상태로 생성
-  return render(request,'posts/post_form.html',{"form":post_form})
+# def post_update(request, post_id):
+#   post = get_object_or_404(Post, id=post_id)
+#   if request.method == 'POST':
+#     post_form = PostForm(request.POST, instance=post)
+#     if post_form.is_valid():
+#       post_form.save()
+#       return redirect('post-detail',post_id = post_id)
+#   else:
+#     post_form = PostForm(instance=post) #post데이터가 폼에 바운드 된 상태로 생성
+#   return render(request,'posts/post_form.html',{"form":post_form})
 
-def post_delete(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  if request.method == 'POST':
-    post.delete()
-    return redirect('post-list')
-  else:
-    return render(request, 'posts/post_confirm_delete.html',{'post':post})
+class PostUpdateView(UpdateView):
+  model = Post
+  form_class = PostForm
+  template_name = 'posts/post_form.html'
+  pk_url_kwarg = 'post_id'
 
-def index(request):
-  return redirect('post-list')
+  def get_success_url(self):
+    return reverse('post-detail', kwargs={'post_id': self.object.id})
+
+# def post_delete(request, post_id):
+#   post = get_object_or_404(Post, id=post_id)
+#   if request.method == 'POST':
+#     post.delete()
+#     return redirect('post-list')
+#   else:
+#     return render(request, 'posts/post_confirm_delete.html',{'post':post})
+
+class PostDeleteView(DeleteView):
+  model = Post
+  template_name = 'posts/post_confirm_delete.html'
+  pk_url_kwarg = 'post_id'
+  context_object_name = 'post'
+  def get_success_url(self):
+    return reverse('post-list')
