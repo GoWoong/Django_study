4b3381f98754df5d84d1093c5a7d9511ac93fb14
GoWoong <popgun10@naver.com>
2022-04-08 15:43:08 +0900

    page design

--- chapter3/coplate_project/coplate/urls.py
+++ chapter3/coplate_project/coplate/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
+    path('', views.IndexView.as_view(), name='index'),
+    path(
+      'revuews/<int:review_id>/',
+      views.ReviewDetailView.as_view(),
+      name='review-detail',
+    )
 ]
