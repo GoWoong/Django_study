0fa50014c6091b29fbba0041028ee841165ca240
GoWoong <popgun10@naver.com>
2022-11-07 21:50:39 +0900

    study migration

--- fast_campus/shrinkrs/shrinkrs/settings.py
+++ fast_campus/shrinkrs/shrinkrs/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortener'
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'shrinkrs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
